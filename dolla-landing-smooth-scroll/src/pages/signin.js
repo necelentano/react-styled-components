95680ac8aed5de551a4ae165b2a65989230453eb
necelentano <necelentano@gmail.com>
2022-08-01 23:22:25 +0300

    Scroll to Top on Page/Route Change

--- dolla-landing-smooth-scroll/src/pages/signin.js
+++ dolla-landing-smooth-scroll/src/pages/signin.js
@@ -1,8 +1,10 @@
 import Signin from '../components/Signin';
+import ScrollToTop from '../components/ScrollToTop';
 
 const SigninPage = () => {
   return (
     <div>
+      <ScrollToTop />
       <Signin />
     </div>
   );
